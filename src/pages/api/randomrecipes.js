c9bb9a9f256799c08b0c606404cbd1dfef0edaea
logankarstens <lkarstens2021@fau.edu>
2023-07-25 18:24:32 -0400

    remove console log

--- src/pages/api/randomrecipes.js
+++ src/pages/api/randomrecipes.js
@@ -4,6 +4,5 @@ export default async function handler(req, res) {
   let response = await fetch(
     `https://api.spoonacular.com/recipes/random?apiKey=${process.env.API_KEY}&number=6`
   );
-  console.log(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.API_KEY}&number=6`)
   res.status(200).json(await response.json());
 }
