900a8f0ab1b1aed347d61b093cd99bff13b91765
logankarstens <lkarstens2021@fau.edu>
2023-07-25 23:21:34 -0400

    fix bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,7 @@ const LandingPage = () => {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl text-center">
             <h1 className="text-4xl font-bold tracking-tight text-black sm:text-6xl">
-<<<<<<< Updated upstream
               Welcome {ctx.user ? ctx.user.displayName : ""} to TasteBudz
-=======
-              Welcome to TasteBudz
->>>>>>> Stashed changes
             </h1>
             <p className="mt-6 text-lg leading-8 text-black ">
               TasteBudz is a premium recipe application that revolutionizes how
